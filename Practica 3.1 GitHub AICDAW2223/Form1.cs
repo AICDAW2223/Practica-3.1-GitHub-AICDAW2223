3c4deff5de37b18ff995d9979f62315f5ac41884
AICDAW2223 <adribacan@alu.edu.gva.es>
2022-11-28 13:43:34 +0100

    Agregar archivos de proyecto.

---
+++ Practica 3.1 GitHub AICDAW2223/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_3._1_GitHub_AICDAW2223
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}

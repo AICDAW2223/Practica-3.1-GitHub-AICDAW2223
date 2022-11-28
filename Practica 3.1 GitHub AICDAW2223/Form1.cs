281c04c15545887cd2984e0cb0cf159c0df0b73e
AICDAW2223 <adribacan@alu.edu.gva.es>
2022-11-28 14:04:12 +0100

    Primer commit

--- Practica 3.1 GitHub AICDAW2223/Form1.cs
+++ Practica 3.1 GitHub AICDAW2223/Form1.cs
@@ -16,5 +16,39 @@ namespace Practica_3._1_GitHub_AICDAW2223
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+                string textoTelegrama;
+                char tipoTelegrama = ' ';
+                int numPalabras = 0;
+                double coste;
+                //Leo el telegrama
+                textoTelegrama = txtTelegrama.Text;
+                // telegrama urgente?
+                if (cbUrgente.Checked)
+                    tipoTelegrama = 'u';
+                //Obtengo el número de palabras que forma el telegrama
+                numPalabras = textoTelegrama.Length;
+                //Si el telegrama es ordinario
+                if (tipoTelegrama == 'o')
+                    if (numPalabras <= 10)
+                        coste = 25;
+                    else
+                        coste = 0.5 * numPalabras;
+                else
+                //Si el telegrama es urgente
+                if (tipoTelegrama == 'u')
+                    if (numPalabras <= 10)
+                        coste = 5;
+                    else
+                        coste = 5 + 0.75 * (numPalabras - 10);
+                else
+                    coste = 0;
+                txtTelegrama.Text = coste.ToString() + " euros";
+            
+
+        }
     }
 }
